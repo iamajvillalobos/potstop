181820e2085ebcc3bc3e10c553386d3b170f1d62
AJ Villalobos <hi@ajvillalobos.com>
2021-10-03 21:13:27 +0800

    show answers

--- components/Account.js
+++ components/Account.js
@@ -1,16 +1,18 @@
-import EthName from './EthName'
+import EthName from "./EthName";
 
-const Account = function () {
+const Account = function ({ accounts, connect, isLoggedIn }) {
   // TODO!!!
-  // if already logged in, it should show 
+  // if already logged in, it should show
   // the EthName component with the correct address
   // if not logged in, show a connect button
   // that when its clicked, will prompt us to login
   // and store the info on the page
 
-  return (
-    <button>Connect</button>
-  )
-}
+  if (isLoggedIn) {
+    return <span>{accounts[0]}</span>;
+  } else {
+    return <button onClick={connect}>Connect</button>;
+  }
+};
 
 export default Account;
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,18 @@
 // for next.js's <head> tag and rendering images
-import Head from 'next/head'
-import Image from 'next/image'
+import Head from "next/head";
+import Image from "next/image";
 
 // import the web3 library with setup from lib/web3.js
-import { web3 } from '../lib/web3';
+import { web3 } from "../lib/web3";
 
 // import react hooks
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 // all from our components folder
-import Account from '../components/Account'
-import EthName from '../components/EthName'
-import Answer from '../components/Answer'
-import AnswerForm from '../components/AnswerForm'
+import Account from "../components/Account";
+import EthName from "../components/EthName";
+import Answer from "../components/Answer";
+import AnswerForm from "../components/AnswerForm";
 
 export default function Home() {
   // todo:
@@ -21,6 +21,60 @@ export default function Home() {
   // 3. add tipping like project 1
   // 4. make the user name look good
   // 5. let the user post their own reply
+
+  const [accounts, setAccounts] = useState([]);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoading, setLoading] = useState(true);
+  const [answers, setAnswers] = useState([]);
+
+  const connect = async function () {
+    let accounts = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
+    setAccounts(accounts);
+  };
+
+  useEffect(
+    function () {
+      if (accounts.length > 0) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+    },
+    [accounts]
+  );
+
+  useEffect(async function () {
+    const accounts = await window.ethereum.request({ method: "eth_accounts" });
+    setAccounts(accounts);
+
+    window.ethereum.on("accountsChanged", function (data) {
+      setAccounts(data);
+    });
+
+    fetch("/api/answers")
+      .then((response) => response.json())
+      .then((data) => {
+        setAnswers(data.answers);
+        setLoading(false);
+      });
+  }, []);
+
+  let answersArea = <div className="loading">Loading answers...</div>;
+
+  if (!isLoading) {
+    answersArea = answers.map(function (answer, index) {
+      return (
+        <Answer
+          number={index + 1}
+          answer={answer}
+          accounts={accounts}
+          isLoggedIn={isLoggedIn}
+        />
+      );
+    });
+  }
 
   return (
     <main>
@@ -31,14 +85,24 @@ export default function Home() {
           <input type="text" placeholder="Search" />
         </form>
 
-        <button>Connect</button>
+        <Account
+          accounts={accounts}
+          connect={connect}
+          isLoggedIn={isLoggedIn}
+        />
       </header>
 
       <section className="question">
         <div className="main">
           <h3>Feedback forum</h3>
           <h2>Looking for feedback as a beginner</h2>
-          <p>Hey everyone, I&apos;m a new potter, just 4 weeks into my journey, and I&apos;m looking to get some feedback on what I&apos;ve made so far. I&apos;m particularly interested in how to make rustic looking bowls and pots, and I&apos;d love to know what the best tools to use would be!</p>
+          <p>
+            Hey everyone, I&apos;m a new potter, just 4 weeks into my journey,
+            and I&apos;m looking to get some feedback on what I&apos;ve made so
+            far. I&apos;m particularly interested in how to make rustic looking
+            bowls and pots, and I&apos;d love to know what the best tools to use
+            would be!
+          </p>
 
           <div className="slides">
             <Image src="/image-1.jpg" width="600" height="800" />
@@ -48,30 +112,37 @@ export default function Home() {
           </div>
         </div>
         <div className="meta">
-          
           {/* EthName */}
           <div className="eth-name">
-            <img src="https://ipfs.io/ipfs/QmbctVN8tPaDLiLysVDwThf7JTJhMejbSypZ4a3v5H2G3a" alt="Avatar of riklomas.eth" />
+            <img
+              src="https://ipfs.io/ipfs/QmbctVN8tPaDLiLysVDwThf7JTJhMejbSypZ4a3v5H2G3a"
+              alt="Avatar of riklomas.eth"
+            />
             <div className="name">
               <span className="primary">riklomas.eth</span>
               <span className="secondary">0xb25bf3...aaf4</span>
             </div>
           </div>
           {/* end EthName */}
-
         </div>
       </section>
 
-      <section className="answers">
-        <div className="loading">Loading answers...</div>
-      </section>
+      <section className="answers">{answersArea}</section>
 
       <Head>
-        <title>Looking for feedback as a beginner – Feedback forum – Potstop </title>
-        <meta property="og:title" content="Looking for feedback as a beginner on Potstop" />
-        <meta property="og:description" content="This is a project on the SuperHi Crypto + Web3 for Creatives course" />
+        <title>
+          Looking for feedback as a beginner – Feedback forum – Potstop{" "}
+        </title>
+        <meta
+          property="og:title"
+          content="Looking for feedback as a beginner on Potstop"
+        />
+        <meta
+          property="og:description"
+          content="This is a project on the SuperHi Crypto + Web3 for Creatives course"
+        />
         <meta property="og:image" content="/social.png" />
       </Head>
     </main>
-  )
+  );
 }
